ded24aee5f397008d43cd306bdbab64d61251341
nikhar-25 <nikharms2500@gmail.com>
2024-03-23 02:00:50 +0530

    changed in init app

--- .flaskenv
+++ .flaskenv
@@ -1,5 +1,4 @@
 export FLASK_ENV=production
 export FLASK_APP=src
-export SQLALCHEMY_DATABASE_URI=sqlite:///bookmarks.db
 // export FLASK_DEBUG=1
 export JWT_SECRET_KEY='JWT_SECRET_KEY'
--- src/__init__.py
+++ src/__init__.py
@@ -13,11 +13,11 @@ from src.config.swagger import template, swagger_config
 def create_app(test_config=None):
 
     app = Flask(__name__, instance_relative_config=True)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookmarks.db'
 
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
-            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"),
             SQLALCHEMY_TRACK_MODIFICATIONS=False,
             JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY'),
 
